bdfd670e7586fc74749bd168c69c7c0379c134fe
Noa <noagoren@post.bgu.ac.il>
2021-01-03 11:54:04 +0200

    exception added

--- server/api/form_submit.py
+++ server/api/form_submit.py
@@ -9,24 +9,18 @@ from api.utils import exceptions_mapper, json_abort, plt_to_src
 
 class FormSubmit(MethodView):
     def post(self):
-        model = Markowitz()
-        dict_variable = {int(key): value for (key, value) in request.json.items()}
-        score = get_risk_horizon_score(dict_variable)
-        print(score)
-        fig = model.get_optimal_portfolio(score)
-        base64image = plt_to_src(fig)
-        #
-        #
-        # expected_fields = ['name']
-        # try:
-        #     name = itemgetter(*expected_fields)(request.form)
-        # except Exception as e:
-        #     json_abort(*exceptions_mapper(400, expected_fields), e)
-        # try:
-        #
-        #     return make_response(jsonify(message="Algorithm added"), 201)
-        # except Exception as e:
-        #     json_abort(*exceptions_mapper(500), e)
+        try:
+            dict_variable = {int(key): value for (key, value) in request.json.items()}
+        except Exception as e:
+            json_abort(*exceptions_mapper(400, []), e)
+        try:
+            model = Markowitz()
+            score = get_risk_horizon_score(dict_variable)
+            print(score)
+            fig = model.get_optimal_portfolio(score)
+            base64image = plt_to_src(fig)
+        except Exception as e:
+            json_abort(*exceptions_mapper(500), e)
         return make_response(jsonify(message="Porfolio", src=base64image), 200)
 
 
