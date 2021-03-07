3d065e4ea22af22959686344e6e693083d401834
MoranChery <chery@post.bgu.ac.il>
2021-03-07 16:59:43 +0200

    Finished users.py, including tests in users_api.py

--- server/models/users.py
+++ server/models/users.py
@@ -13,9 +13,16 @@ class User(db.Model):
     first_name = db.Column('first_name', db.String)
     last_name = db.Column('last_name', db.String)
     age = db.Column('age', db.Numeric)
-    sex = db.Column('sex', db.Enum(Gender))
+    gender = db.Column('gender', db.Enum(Gender))
     latest_portfolio_risk = db.Column('latest_portfolio_risk', db.Enum(Risk), default='undefined')
 
     def as_dict(self):
-        user_as_dict = {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        user_as_dict = {
+            'email': self.email,
+            'first_name' : self.first_name,
+            'last_name': self.last_name,
+            'age': str(self.age),
+            'gender': self.gender.name,
+            'latest_portfolio_risk': self.latest_portfolio_risk.name
+        }
         return user_as_dict
