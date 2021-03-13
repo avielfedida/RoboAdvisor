9538679c375eae20292e0b59d6afb51b6e6d7d8a
Aviel Fedida <avielfedida@gmail.com>
2021-03-13 22:20:34 +0200

    Update init.py
    Changed location to desktop

--- server/init.py
+++ server/init.py
@@ -8,7 +8,7 @@ pc_user = "Noa"
 
 to_run = [
     (redis_init, '/redis-celery/',),
-    (celery_init, f'/Users/{pc_user}/PycharmProjects/RoboAdvisor/server/'),
+    (celery_init, f'/Users/{pc_user}/Desktop/RoboAdvisor/server/'),
 ]
 
 # https://stackoverflow.com/questions/30686295/how-do-i-run-multiple-subprocesses-in-parallel-and-wait-for-them-to-finish-in-py
