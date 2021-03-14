b6020ee5c7e81892af8acf08fad099c83c00d3fb
Aviel Fedida <avielfedida@gmail.com>
2021-03-14 19:52:51 +0200

    Added beat schedule, for now print hello world every 2 seconds

--- server/init.py
+++ server/init.py
@@ -11,7 +11,7 @@ to_run = [
     (redis_init, '/redis-celery/',),
     # The order is important: https://stackoverflow.com/questions/9769496/celery-received-unregistered-task-of-type-run-example
     (celery_init, f'/Users/{pc_user}/Desktop/RoboAdvisor/server/'),
-    (celery_beat, f'/Users/{pc_user}/Desktop/RoboAdvisor/server/'),
+    # (celery_beat, f'/Users/{pc_user}/Desktop/RoboAdvisor/server/'),
 ]
 
 # https://stackoverflow.com/questions/30686295/how-do-i-run-multiple-subprocesses-in-parallel-and-wait-for-them-to-finish-in-py
