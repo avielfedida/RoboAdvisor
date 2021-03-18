6ef1cb4f802fef5bd48fa377df8b8b45561134d2
Noa <noagoren@post.bgu.ac.il>
2021-03-18 13:31:03 +0200

    added more checks to register

--- server/api/users_api.py
+++ server/api/users_api.py
@@ -42,9 +42,20 @@ class UsersApi(MethodView):
 class RegisterUser(MethodView):
     def post(self):
         data = request.get_json()
+        # first time in the system
         if data.get("_id") is None:
-            self.createNewUser(data)
-            self.createMember(data)
+            user_email = data.get("email")
+            check_if_member_in_db = db.session.query(Member).filter_by(user_id=user_email).first()
+            if check_if_member_in_db:
+                json_abort(409, "Member already exist")
+            check_if_user_in_db = db.session.query(User).filter_by(_id=user_email).first()
+            # if has user in db that its the email
+            if check_if_user_in_db:
+                self.createMember(data)
+            # not exist in no db
+            else:
+                self.createNewUser(data)
+                self.createMember(data)
             response = make_response(jsonify(message="User successfully added to database"), 200)
         # the user submit the form at least one time
         else:
@@ -52,7 +63,7 @@ class RegisterUser(MethodView):
             user_mail = data.get("email")
             user_from_db = db.session.query(User).filter_by(_id=user_id).first()
             user_from_db._id = user_mail
-            member_in_db = db.session.query(Member).filter_by(_id=user_mail).first()
+            member_in_db = db.session.query(Member).filter_by(user_id=user_mail).first()
             if member_in_db is None:
                 self.createMember(data)
                 response = make_response(jsonify(message="User successfully added to database"), 200)
@@ -63,12 +74,17 @@ class RegisterUser(MethodView):
 
     def createMember(self, data):
         user_email = data.get("email")
+        # check_if_in_member_db = db.session.query(Member).filter_by(user_id=user_email).first()
+        # if check_if_in_member_db:
+        #     json_abort(409, "Member already exist")
         user_password = data.get("password")
         user_first_name = data.get("first_name")
         user_last_name = data.get("last_name")
         user_age = data.get("age")
         user_gender = data.get("gender")
         user_id = user_email
+        if not user_email or not user_password or not user_first_name or not user_last_name or not user_age or not user_gender:
+            json_abort(400, "Missing on or more fields")
         new_member = Member(user_email, user_password, user_first_name, user_last_name, user_age, user_gender, user_id)
         db.session.add(new_member)
         db.session.commit()
