bf6e41e3a58b1f352dfe8eae2c2b63807517e4b0
Noa <noagoren@post.bgu.ac.il>
2021-05-13 12:24:01 +0300

    unit tests for users_api

---
+++ server/tests/Units_test/users_api_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+from ..test_base import TestBase
+
+
+class UsersTest(TestBase):
+    def setUp(self):
+        super(UsersTest, self).setUp()
+        self.base += '/users'
+        self.username1 = 'aviel@gmail.com'
+        self.password1 = 'pass'
+
+    def tearDown(self):
+        super(UsersTest, self).tearDown()
+
+    def test_register_all_valid(self):
+        res = self.client().post(f'{self.base}/register',
+                                 json=dict(email=self.username1, password=self.password1, first_name="avi",
+                                           last_name="yoni"), headers=self.headers)
+        self.assertEqual(200, res.status_code)
+
+    def test_register_all_twice_same_username(self):
+        res = self.client().post(f'{self.base}/register',
+                                 json=dict(email=self.username1, password=self.password1, first_name="avi",
+                                           last_name="yoni"), headers=self.headers)
+        self.assertEqual(200, res.status_code)
+        res = self.client().post(f'{self.base}/register',
+                                 json=dict(email=self.username1, password=self.password1, first_name="avi",
+                                           last_name="yoni"), headers=self.headers)
+        self.assertEqual(409, res.status_code)
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
